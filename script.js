55eadee2d61cc6de22f49654fd3d8f1ab92fd508
PC69 <jamadarayesh9@gmail.com>
2025-08-01 12:51:05 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+function openMessage() {
+  document.getElementById("message").style.display = "block";
+}
+
+function closeMessage() {
+  document.getElementById("message").style.display = "none";
+}
